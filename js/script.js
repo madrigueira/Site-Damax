f0c385ccbcab325e02ce84f2005793296c499346
unknown <mndias29@gmail.com>
2022-10-03 16:53:38 -0300

    fazendo certo agr, bloco 3 pronto (só falta mais um nessa pagina)

---
+++ js/script.js
@@ -0,0 +1,81 @@
+
+//  Chama os blocos da pasta html pras páginas principais
+$(function(){
+    $("#header-1").load("html/header-1.html");
+    $("#header-2").load("html/header-2.html");
+    $("#footer").load("html/footer.html");
+});
+
+
+//  Menu Mobile
+function toggleMenu(){
+    document.getElementById("menu").classList.toggle("active")
+}
+
+// Iniciar biblioteca de animações
+AOS.init({
+    offset: 200,
+    once: true,
+    duration: 600,
+});
+
+//  Carousel da Home - Bloco 2
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 40,
+    loop: true,
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+     },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        850: {
+            slidesPerView: 3,
+        },
+        600: {
+            slidesPerView: 2,
+        }
+    }
+});
+
+
+// Scroll do Quem Somos - Bloco 2 (meu Deus eu fiquei muito tempo nksso....)
+function toggleScroll(){
+    var card = document.getElementById('card')
+    var btn = document.getElementById('btn-scroll')
+    if(card.scrollTop == 0){
+        card.scroll({
+            top: 1000,
+            behavior: 'smooth'
+        })
+    }else{
+        card.scroll({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+    btn.classList.toggle("active")
+}
+
+function toggleScroll_2(){
+    var card = document.getElementById('card-2')
+    var btn = document.getElementById('btn-scroll-2')
+    if(card.scrollTop == 0){
+        card.scroll({
+            top: 1000,
+            behavior: 'smooth'
+        })
+    }else{
+        card.scroll({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+    btn.classList.toggle("active")
+}
+
